df36a42c1c90ea7505e375c4ee29264f0f2a2c8d
kpk666 <kpk666@tut.by>
2023-03-16 14:10:17 +0100

    Files update

--- deploy/docker_deploy.sh
+++ deploy/docker_deploy.sh
@@ -2,12 +2,12 @@
 
 # Docker version
 echo "Check docker version"
-sudo docker version
+docker version
 
 # Docker-compose version
 echo "Check docker-compose version"
-sudo docker-compose version
+docker-compose version
 
 # Launch docker-compose
 echo "Launch project"
-sudo docker-compose up -d
+docker-compose up -d
